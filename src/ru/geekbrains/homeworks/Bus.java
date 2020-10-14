e473541524cbedda18ff2c565d91d62ea02a41a1
Kosenko <Kosenko.2000@yandex.ru>
2020-10-15 01:35:04 +0500

    lvl3 lesson5

---
+++ src/ru/geekbrains/homeworks/Bus.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.homeworks;
+
+public class Bus extends Mashins{
+
+    public Bus(String name, FuelStation fuelStation) {
+        super(name, 40, 7.5f, fuelStation);
+    }
+}
---
+++ src/ru/geekbrains/homeworks/Truck.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.homeworks;
+
+public class Truck extends Mashins{
+
+    public Truck(String name,FuelStation fuelStation) {
+        super(name, 60, 7.5f, fuelStation);
+    }
+}
+
